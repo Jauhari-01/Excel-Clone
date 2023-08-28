74a8e181012ea13c1f860a5ce286eb5c7648a6df
Jauhari-01 <shubham.jauhari009@gmail.com>
2023-08-28 17:52:29 +0530

    Completed functionalities

--- script.js
+++ script.js
@@ -20,10 +20,19 @@ const rows = 100;
 
 //
 let currentCell;
-let copyObj = {
+let copyObj = {}
+let cutPres = false;
+let matrix = new Array(rows);
 
+//creating matrix for daunload
+for(let row = 0 ; row < rows ; row++){
+    matrix[row] = new Array(columns);
+    for(let col = 0 ; col < columns ; col++){
+        matrix[row][col] = {};
+    }
 }
 
+// console.log(matrix);
 
 //table related stuff
 for(let col=0 ; col < columns  ; col++){
@@ -43,6 +52,7 @@ for(let row = 1 ; row<=rows ; row++){
         td.setAttribute('contenteditable','true');
         td.setAttribute('id',`${String.fromCharCode(col+65)}${row}`)
         td.addEventListener('focus',(event)=>onFocusFn(event));
+        td.addEventListener('input',(event)=>onChangeFn(event));
         tr.appendChild(td);
     }
     tBody.append(tr);
@@ -52,6 +62,21 @@ function onFocusFn(event){
     currentCell = event.target;
     document.getElementById('current-cell').innerText = currentCell.id;
 }
+function onChangeFn(event){
+    updateMatrix(event.target);
+}
+function updateMatrix(currentCell){
+    let tempObj = {
+        style:currentCell.style.cssText,
+        text: currentCell.innerText,
+        id: currentCell.id
+    }
+    let col = currentCell.id.charCodeAt(0)-65;
+    let row = currentCell.id.substr(1)-1;
+    // console.log(col);
+    matrix[row][col] = tempObj;
+}
+
 
 //functions for editing
 
@@ -63,30 +88,37 @@ boldButton.addEventListener('click',()=>{
     //     currentCell.style.fontWeight = 'bold';
 
     currentCell.style.fontWeight = currentCell.style.fontWeight === 'bold' ? 'normal':'bold';
+    updateMatrix(currentCell);
 })
 
 //Italics button
 italicsButton.addEventListener('click',()=>{
     currentCell.style.fontStyle = currentCell.style.fontStyle === 'italic' ? 'normal':'italic';
+    updateMatrix(currentCell);
 })
 
 //underline button
 underLineButton.addEventListener('click',()=>{
     currentCell.style.textDecoration = currentCell.style.textDecoration === 'underline' ? 'none':'underline';
+    updateMatrix(currentCell);
 })
 
 //Align content related functions
 leftAlignBtn.addEventListener('click',()=>{
     currentCell.style.textAlign = currentCell.style.textAlign === 'left' ? 'none':'left';
+    updateMatrix(currentCell);
 })
 centerAlignBtn.addEventListener('click',()=>{
     currentCell.style.textAlign = currentCell.style.textAlign === 'center' ? 'left':'center';
+    updateMatrix(currentCell);
 })
 rightAlignBtn.addEventListener('click',()=>{
     currentCell.style.textAlign = currentCell.style.textAlign === 'right' ? 'left':'right';
+    updateMatrix(currentCell);
 })
 justifyAlignBtn.addEventListener('click',()=>{
     currentCell.style.textAlign = currentCell.style.textAlign === 'justify' ? 'left':'justify';
+    updateMatrix(currentCell);
 })
 
 
@@ -94,20 +126,24 @@ justifyAlignBtn.addEventListener('click',()=>{
 //changing font size
 fontSizeDropDown.addEventListener('change',()=>{
     currentCell.style.fontSize = fontSizeDropDown.value;
+    updateMatrix(currentCell);
 });
 
 //font family
 fontStyleDropDown.addEventListener('change',()=>{
     currentCell.style.fontFamily = fontStyleDropDown.value;
+    updateMatrix(currentCell);
 });
 
 //bg color
 bgColorInput.addEventListener('input',()=>{
     currentCell.style.backgroundColor = bgColorInput.value;
+    updateMatrix(currentCell);
 })
 //font color
 fontColorInput.addEventListener('input',()=>{
     currentCell.style.color = fontColorInput.value;
+    updateMatrix(currentCell);
 })
 
 
@@ -117,16 +153,45 @@ copyContentbtn.addEventListener('click',()=>{
         style : currentCell.style.cssText,
         text : currentCell.innerText
     }
+    cutPres = true;
+    updateMatrix(currentCell);
 })
 cutContentbtn.addEventListener('click',()=>{
     copyObj ={
         style : currentCell.style.cssText,
         text : currentCell.innerText
     }
+    cutPres = true;
     currentCell.style.cssText = "";
     currentCell.innerText = '';
+    updateMatrix(currentCell);
 })
 pasteContentbtn.addEventListener('click',()=>{
-    currentCell.style.cssText = copyObj.style;
-    currentCell.innerText = copyObj.text;
+    if(cutPres){
+        currentCell.style.cssText = copyObj.style;
+        currentCell.innerText = copyObj.text;
+        updateMatrix(currentCell);
+    }
 })
+
+//dawnload btn
+function downloadJson(){
+    const str = JSON.stringify(matrix);
+
+    //text form of matrix --> piece of memory(downloadable)
+    // application/json -> format for json
+    const blob = new Blob([str],{type:'application/json'});
+
+
+    //link created -> attach href
+    //click link
+    //delete link
+
+    // now i need to create a dynamic link
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
